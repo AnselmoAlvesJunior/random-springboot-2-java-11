d00e2aa38fd55beb478f488297604127458f765a
Anselmo Alves Junior <ansjunior@msn.com>
2020-04-03 17:27:23 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projectweb/random/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.projectweb.random.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projectweb.random.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+	
+	
+}

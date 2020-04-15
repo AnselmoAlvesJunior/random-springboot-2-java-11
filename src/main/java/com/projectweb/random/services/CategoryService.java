3249bd385ead596d0d7dbc283ab0c7baae5aa03e
Anselmo Alves Junior <ansjunior@msn.com>
2020-04-15 19:32:45 -0300

    Category

---
+++ src/main/java/com/projectweb/random/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.projectweb.random.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projectweb.random.entities.Category;
+import com.projectweb.random.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll(); 
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj=repository.findById(id);
+		return obj.get();
+	}
+}

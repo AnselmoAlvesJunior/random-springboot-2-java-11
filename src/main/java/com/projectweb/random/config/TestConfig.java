3de3e520ec3c1446ecc8160166659614f08f8974
Anselmo Alves Junior <ansjunior@msn.com>
2020-04-15 21:46:39 -0300

    Payment, one-to-one association

--- src/main/java/com/projectweb/random/config/TestConfig.java
+++ src/main/java/com/projectweb/random/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectweb.random.entities.Category;
 import com.projectweb.random.entities.Order;
 import com.projectweb.random.entities.OrderItem;
+import com.projectweb.random.entities.Payment;
 import com.projectweb.random.entities.Product;
 import com.projectweb.random.entities.User;
 import com.projectweb.random.entities.enums.OrderStatus;
@@ -78,5 +79,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		Payment pay1=new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
